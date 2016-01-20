a798588a6223f812c269699ca9b4d3e776499414
Amr Noman <amr.noman1990@gmail.com>
2016-01-21 00:05:19 +0300

    before parallax

--- app/assets/js/shared/index.js
+++ app/assets/js/shared/index.js
@@ -2,6 +2,7 @@ var app = require('angular').module('myApp');
 app.directive("fixOnScroll", require("./fixOnScrollDirective"));
 app.directive("scrollOnClick", require("./scrollOnClickDirective"));
 app.directive("scrollFollow", require("./scrollFollowDirective"));
+// app.directive("parallax", require("./parallaxDirective"));
 
 
 require("./onScrollClass");
